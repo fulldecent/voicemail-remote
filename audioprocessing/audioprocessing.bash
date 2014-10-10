b4a334a2580bf3a42c904121057aa4dadb7b07db
William Entriken <github.com@phor.net>
2014-10-10 18:34:25 -0400

    Update directories

--- audioprocessing/audioprocessing.bash
+++ audioprocessing/audioprocessing.bash
@@ -10,21 +10,28 @@
 
 
 #
+# Clean up
+#
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+rm -f $DIR/processed.wav $DIR/processed.raw $DIR/synthesized.aif
+
+
+#
 # Synthesize the message from STDIN
 #
 
-cat $1 > say -o synthesized.aif
-#cat $1 > text2wave -o synthesized.aif
+cat $1 | say -o $DIR/synthesized.aif
+#cat $1 | text2wave -o $DIR/synthesized.aif
 
 
 #
 # Now concatenate all files in script to one audio
 #
 
-rm -f ./processed.wav ./processed.raw
 for a in $(grep -v "#" $2)
 do
-  sox $a.* -t raw -r 44100 -c1 -e signed-integer - >> ./processed.raw
+  sox $DIR/$a.* -t raw -r 44100 -c1 -e signed-integer - >> $DIR/processed.raw
 done
 
 
@@ -33,11 +40,12 @@ done
 # https://developer.skype.com/jira/browse/SPA-150
 #
 
-sox -t raw -e signed-integer -b16 --endian little -r 44100 -c 1 ./processed.raw -t wav -r 16000 ./processed.wav
+sox -t raw -e signed-integer -b16 --endian little -r 44100 -c 1 $DIR/processed.raw -t wav -r 16000 $DIR/processed.wav
 
 
 #
 # Clean up
 #
-rm -f ./processed.raw ./synthesized.aif
+
+rm -f $DIR/processed.raw $DIR/synthesized.aif
 
